b8d22f2b54d2a925b9fedd8d2f0e71efda5a7962
Eli Pagan <mbk8689@gmail.com>
2021-12-16 18:46:16 -0500

    added fetch request in js

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.static('public'));
+
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
@@ -109,6 +112,10 @@ app.post('/api/animals', (req, res) => {
         const animal = createNewAnimal(req.body, animals);
         res.json(req.body);
     }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
